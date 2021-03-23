ed0235318ba8563ecd3b9d90cb492b1dc93411af
mili <miiladmohammadii@gmail.com>
2021-03-23 10:02:23 +0000

    remove old file

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '+-$tn75!0fnzny70vakwj8tggh+h-d=&w11z-%@(cp!8r4j!pm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['miiladmohamadii.pythonanywhere.com']
 
 
 # Application definition
@@ -120,5 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL  = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
